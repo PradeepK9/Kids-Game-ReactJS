0c0f8341237ee20f941273d42dd887434eb1e6eb
Pradeep Jaiswal <pradeep88318@mastek.com>
2022-09-21 18:18:24 +0530

    added all mobile features to deskop view

--- src/components/MosquitoKillerDesktop.jsx
+++ src/components/MosquitoKillerDesktop.jsx
@@ -5,6 +5,7 @@ const MosquitoKillerDesktop = () => {
     const [hitCount, setHitCount] = useState(0);
     const [gameStatus, setGameStatus] = useState("");
     const [isStarted, setIsStarted] = useState(false);
+    const [gameOver, setGameOver] = useState(false);
     const [timer, setTimer] = useState("");
     const [startBtnText, setStartBtnText] = useState("Start Game");
     const [timeIntervals, setTimeIntervals] = useState([]);
@@ -19,6 +20,7 @@ const MosquitoKillerDesktop = () => {
 
     const handleChange = (e) => {
         setIsStarted(false);
+        setGameOver(false);
         setHitCount(0);
         setTimer("");
         setStartBtnText("Start Game");
@@ -31,6 +33,7 @@ const MosquitoKillerDesktop = () => {
     const startGame = () => {
         clearAllTimeOuts();
         setIsStarted(true);
+        setGameOver(false);
         setHitCount(0);
         setGameStatus("Game Started....");
         setStartBtnText("Restart Game");
@@ -43,7 +46,7 @@ const MosquitoKillerDesktop = () => {
             totalSeond--;
             setTimer(`00:00:${totalSeond}`);
         }, 1000);
-        
+
         setTimeIntervals((items) => items.concat(timer));
 
         const time = setInterval(() => {
@@ -54,17 +57,18 @@ const MosquitoKillerDesktop = () => {
             mosquito.style.top = j + 'px';
         }, value);
 
-        
+
         setTimeIntervals((items) => items.concat(time));
 
         const timeout = setTimeout(() => {
             setGameStatus("Game Over!");
             statusEle.style.color = "red";
+            setGameOver(true);
             clearTimeout(time);
             clearTimeout(timer);
             setIsStarted(false);
         }, 30000);
-        
+
         setTimeIntervals((items) => items.concat(timeout));
     }
 
@@ -78,12 +82,31 @@ const MosquitoKillerDesktop = () => {
         <div className="MosquitoKillerDesktop">
             <div className="Playground">
                 <img id='mosquito' onClick={hitCounter} src="images/mosquito.png" alt="mosquito" />
+                {gameOver &&
+                    <div className='GameOverDesktop'>
+                        <h1>Game Over !</h1>
+                        <h2>You Scored : {hitCount}</h2>
+                        <h2>Try Again</h2>
+                    </div>
+                }
+                {!isStarted && !gameOver &&
+                    <div className='HowToSectionDesk'>
+                        <p>How to play ?</p>
+                        <p>Click the Start Game button.</p>
+                        <p>And start clicking on mosquito to kill it.</p>
+                        <p>You will have only 30 seconds at a time.</p>
+                        <p>There are 3 difficulty levels : easy, medium and hard.</p>
+                        <p>If you want you can change the difficulty levels.</p>
+                        <p>For best experiene use mobile device to play.</p>
+                        <p>Happy Gamming !</p>
+                    </div>
+                }
                 {gameStatus === "Game Over" &&
                     <h1>Game Over !</h1>
                 }
             </div>
             <div className="ScoreSection">
-                <div className='level'>
+                <div className='Desktoplevel'>
                     <select value={value} onChange={handleChange}>
                         <option value="1600">Easy</option>
                         <option value="1000">Mediam</option>
